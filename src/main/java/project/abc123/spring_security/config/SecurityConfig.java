bb6aac7aef113ae676ce010177eb4994770900fa
leegwangje <ze105@naver.com>
2025-03-20 17:32:57 +0900

    Http basic 인증 구현 사용자 정보는 메모리안에 저장

--- src/main/java/project/abc123/spring_security/config/SecurityConfig.java
+++ src/main/java/project/abc123/spring_security/config/SecurityConfig.java
@@ -1,4 +1,56 @@
 package project.abc123.spring_security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests() // URL 기반 인가 설정
+                . antMatchers("/user/**").hasRole("USER") // USER 권한 사용자만 접근 가능
+                . antMatchers("/admin/**").hasRole("ADMIN") // ADMIN 권한 사용자만 접근 가능
+                . antMatchers("/logout").authenticated() // 인증 받은 사용자만 접근 가능
+                . antMatchers("/**").permitAll() // 인증/인가 여부와 상관없이 접근 가능
+                .and()
+                .logout() // 로그아웃 설정
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 URL 설정
+                .logoutSuccessHandler((req,res,auth) -> {
+                    res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);  // 로그아웃을 위해 401 응답
+                })
+                .invalidateHttpSession(true) // 세션 무효화
+                .deleteCookies("JSESSIONID") // JSESSIONID 쿠기 삭제
+                .permitAll()
+                .and()
+                .httpBasic(); // HTTP Basic 인증 사용
+        return  http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
--- src/main/java/project/abc123/spring_security/controller/indexController.java
+++ src/main/java/project/abc123/spring_security/controller/indexController.java
@@ -1,4 +1,25 @@
 package project.abc123.spring_security.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class indexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+    // User, Admin 접근 가능
+    @GetMapping("/user")
+    public String user() {
+        return "Hello User";
+    }
+
+    // Admin 접근 가능
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin";
+    }
 }
