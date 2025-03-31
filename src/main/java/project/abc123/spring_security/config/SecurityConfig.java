7aa428dd387eaf7fb185b868fbc666f274413a02
leegwangje <ze105@naver.com>
2025-03-31 11:26:19 +0900

    Jwt 의존성 추가 Jwt 비밀키, 만료시간 설정 Jwt 관련 필터 설정 SecurityConfig에 jwt 필터 설정 JwtAuthenticationFilter 구현 JwtTokenProvider 구현

--- src/main/java/project/abc123/spring_security/config/SecurityConfig.java
+++ src/main/java/project/abc123/spring_security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,18 +13,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import project.abc123.spring_security.jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    //private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserDetailsService userDetailsService;
 
     // securityFilterChain : 스프링 시큐리티에서 적용된 보얀규칙들을 필터로 구현해 둔 것
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf().disable()   // CSRF 필터 끔
@@ -35,21 +40,31 @@ public class SecurityConfig {
                 . antMatchers("/**","/member/**").permitAll() // 인증/인가 여부와 상관없이 접근 가능
 
             .and()
-                .formLogin()  // form login인증 사용
-                .loginPage("/member/login") // 커스텀 로그인 페이지 경로
-                .usernameParameter("userid") // 아이디 매개변수 지정
-                .passwordParameter("passwd") // 비밀번호 매개변수 지정
-                .defaultSuccessUrl("/")  // 로그인 성공시 리다이렉트 URL
-                .failureUrl("/login?error=true") // 로그인 실패시 리다이렉트 URL
-                .permitAll()
+                .sessionManagement() // JWT 인증을 위해 STATELESS 설정
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-            .and()
-                .logout() // 로그아웃 설정
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 URL 설정
-                .logoutSuccessUrl("/") // 로그아웃 성공 후 리다이렉트될 URL
-                .invalidateHttpSession(true) // 세션 무효화
-                .deleteCookies("JSESSIONID") // JSESSIONID 쿠기 삭제
-                .permitAll();
+        // JTW 필터 설정
+        http.addFilterBefore(jwtAuthenticationFilter ,
+                UsernamePasswordAuthenticationFilter.class);
+
+
+
+
+//                .formLogin()  // form login인증 사용
+//                .loginPage("/member/login") // 커스텀 로그인 페이지 경로
+//                .usernameParameter("userid") // 아이디 매개변수 지정
+//                .passwordParameter("passwd") // 비밀번호 매개변수 지정
+//                .defaultSuccessUrl("/")  // 로그인 성공시 리다이렉트 URL
+//                .failureUrl("/login?error=true") // 로그인 실패시 리다이렉트 URL
+//                .permitAll()
+//
+//            .and()
+//                .logout() // 로그아웃 설정
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 URL 설정
+//                .logoutSuccessUrl("/") // 로그아웃 성공 후 리다이렉트될 URL
+//                .invalidateHttpSession(true) // 세션 무효화
+//                .deleteCookies("JSESSIONID") // JSESSIONID 쿠기 삭제
+//                .permitAll();
 
         return  http.build();
     }
